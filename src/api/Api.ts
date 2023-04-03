d9a8269afec59805bf8f0f56f4dbfe1224e2da00
Dion Ng <dionng1998@gmail.com>
2023-04-03 14:45:53 +0800

    refactored code

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -1,3 +1,4 @@
 export const LOGIN_URL = "http://13.211.188.192/user/login";
 export const SIGNUP_URL = "http://13.211.188.192/user/register";
 export const HIDDENCONTENT_URL = "http://13.211.188.192/user/hiddencontent";
+//
